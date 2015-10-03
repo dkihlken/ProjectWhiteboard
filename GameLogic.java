5ddceae829f6904a38dcc0be6a2b83b2827bf76d
dkihlken <dkihlken@iastate.edu>
2015-10-02 23:47:59 -0500

    stuf

--- GameLogic.java
+++ GameLogic.java
@@ -4,4 +4,30 @@ import java.util.ArrayList;
 
 public class GameLogic {
 	ArrayList <BadGuy> BGlist=  new ArrayList<BadGuy>();
+	ArrayList <Tower> Tlist = new ArrayList<Tower>();
+	
+	Map map;
+	
+	public GameLogic(Map m){
+		map = m;
+	}
+	
+	public void step(){
+		int i;
+		for(i=0;i<Tlist.size();i++){
+			if(Tlist.get(i).timeToFire())
+				Tlist.get(i).fire(BGlist);
+			else
+				Tlist.get(i).incrementTime();
+		}
+		for(i=0;i<BGlist.size();i++){
+			if(BGlist.get(i).getHealth() < 0){
+				BGlist.remove(i);
+				i--;
+			}else{
+				BGlist.get(i).move(map);
+			}
+		}
+		
+	}
 }
--- Tower.java
+++ Tower.java
@@ -1,5 +1,20 @@
 package ProjectWhiteboard;
 
-public class Tower {
+import java.util.ArrayList;
+
+public class Tower extends Entity{
+
+	public boolean timeToFire() {
+		return false;
+	}
+
+	public void fire(ArrayList <BadGuy> bglist) {
+		//find target and decrement its health and reset timer
+	}
+
+	public void incrementTime() {
+		//modify timer for firing
+		
+	}
 
 }
